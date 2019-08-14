fc3f3d049eb2746b9e7eabab430427295c426072
MS Mahaye <21609594@dut4life.ac.za>
2019-08-14 13:21:06 +0200

    Signed-off-by: MS Mahaye <21609594@dut4life.ac.za>

--- ModelsLogic/Appointment.cs
+++ ModelsLogic/Appointment.cs
@@ -21,7 +21,7 @@ namespace ModelsLogic
         public System.DateTime Start { get; set; }
         public Nullable<System.DateTime> End { get; set; }
 
-        [Display(Name = "Urgency")]
+        [Display(Name = "Urgencie")]
         public string ThemeColor { get; set; }
 
         public bool IsFullDay { get; set; }
